8c784e5261373fa81ef4b2406eb4cca051b9263c
Julian Nicastro <julrodriguez@itba.edu.ar>
2019-06-17 19:34:09 -0300

    Refactor names to standard ones

--- lib/PokeWidget.dart
+++ lib/PokeWidget.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 
 class PokeWidget extends StatefulWidget {
 
-  final String pokename;
-  final String pokespriteurl;
-  final int pokenumber;
+  final String name;
+  final String spriteUrl;
+  final int number;
 
   PokeWidget({
-    @required this.pokename,
-    @required this.pokenumber,
-    @required this.pokespriteurl
+    @required this.name,
+    @required this.number,
+    @required this.spriteUrl
   });
 
   @override
@@ -35,9 +35,9 @@ class _PokeWidgetState extends State<PokeWidget> {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Text('${widget.pokename}'),
-              Image.asset(widget.pokespriteurl),
-              Text('# ${widget.pokenumber}'),
+              Text('${widget.name}'),
+              Image.asset(widget.spriteUrl),
+              Text('# ${widget.number}'),
             ],
           ),
         ),
