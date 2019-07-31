fce8121ddfa97af3a646117624add66be0819923
Julian Nicastro <julrodriguez@itba.edu.ar>
2019-07-31 19:53:58 -0300

    Connect livingdexList screen with db

--- lib/screens/LivingdexListScreen.dart
+++ lib/screens/LivingdexListScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'LivingdexScreen.dart';
+import 'package:living_dex_tracker/data/Repository.dart';
+import 'package:living_dex_tracker/model/Livingdex.dart';
 import 'CreateLivingdexScreen.dart';
 import '../widgets/LivingdexListElement.dart';
 
@@ -7,18 +8,32 @@ class LivingdexListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: <Widget>[
-            GestureDetector(
-            child: LivingdexListElement(name: 'Jorge', type: 'Regional', game: 'Ruby', totalPokemon: 253),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LivingdexScreen()),
-              );
-            },
-          ),
-        ],
+        body:
+        FutureBuilder<List<Livingdex>>(
+          future: Repository.get().listLivingdexes(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting: return Text('Loading Livingdexes');
+              default:
+                if (snapshot.hasError)
+                  return Text('Oops, something wrong happened. Please Reload the app.');
+                else {
+                  List<Livingdex> livingdexes = snapshot.data;
+                  return ListView.builder(
+                    itemCount: livingdexes.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var livingdex = livingdexes[index];
+                      return new LivingdexListElement(
+                        name: livingdex.name,
+                        game: 'emerald',
+                        totalPokemon: livingdex.pokemons.length,
+                        type: livingdex.shiny ? 'shiny' : 'regular',
+                      );
+                    }
+                  );
+                }
+            }
+          },
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
