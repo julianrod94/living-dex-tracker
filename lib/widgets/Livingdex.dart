8be045ac8d4160638afc8bc7c66ae18ff074441a
jfporrino <juan.f.porrino@hotmail.com>
2019-08-12 15:10:03 -0300

    change box features and style

--- lib/widgets/Livingdex.dart
+++ lib/widgets/Livingdex.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:living_dex_tracker/data/Repository.dart';
 import 'package:living_dex_tracker/model/Pokemon.dart';
 import 'package:living_dex_tracker/widgets/MiniatureBoxElement.dart';
+import 'package:living_dex_tracker/widgets/LivingdexElement.dart';
 import 'package:living_dex_tracker/model/Livingdex.dart' as LivingdexModel;
 
 
@@ -41,43 +42,40 @@ class LivingdexState extends State<Livingdex> {
         itemCount: pokemons.length~/30+1,
         itemBuilder: (BuildContext context,int index1){
           return Padding(
-            padding:const EdgeInsets.all(4.0),
-            child: Card(
-              child: Container(
-                height: 290,
-                width: 50,
-                child: GestureDetector(
-                  child: GridView.count(
-                    crossAxisCount: 6,
-                    children: List.generate(grids[index1].length, (index2) {
-                      return MiniatureBoxElement(
-                        pokemon: grids[index1][index2],
-                        isShiny: livingdex.shiny,
-                        livingdexId: widget.livingdexId,
-                        wasCaptured: grids[index1][index2].captured,
-                      );
-                    }),
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Center(
+                    child: Text(
+                        "Box ${index1}",
+                        style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+                    )
+                ),
+                Card(
+                  child: Container(
+                    height: 290,
+                    width: 50,
+                    child: GridView.count(
+                        physics: new NeverScrollableScrollPhysics(),
+                        crossAxisCount: 6,
+                        children: List.generate(grids[index1].length, (index2) {
+                          return MiniatureBoxElement(
+                            pokemon: grids[index1][index2],
+                            isShiny: livingdex.shiny,
+                            livingdexId: widget.livingdexId,
+                            wasCaptured: grids[index1][index2].captured,
+                          );
+                        }),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
           );
         },
       );
-      /*return GridView.count(
-        crossAxisCount: 3,
-        children: List.generate(30, (index) {
-          return LivingdexElement(
-            pokemon: pokemons[index],
-            isShiny: livingdex.shiny,
-            livingdexId: widget.livingdexId,
-            wasCaptured: pokemons[index].captured,
-            onCaptureStatusChange: (bool captured) =>
-                this.setState(() =>
-                livingdex.pokemons[index].captured = captured),
-          );
-        }),
-      );*/
     }
   }
 }
--- lib/widgets/MiniatureBoxElement.dart
+++ lib/widgets/MiniatureBoxElement.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:living_dex_tracker/model/Pokemon.dart';
+import 'package:living_dex_tracker/data/Repository.dart';
 
 class MiniatureBoxElement extends StatefulWidget {
 
@@ -8,12 +9,14 @@ class MiniatureBoxElement extends StatefulWidget {
   final bool isShiny;
   final int livingdexId;
   final bool wasCaptured;
+  final onCaptureStatusChange;
 
   MiniatureBoxElement({
     @required this.pokemon,
     @required this.isShiny,
     @required this.livingdexId,
     @required this.wasCaptured,
+    @required this.onCaptureStatusChange,
   });
 
   @override
@@ -25,24 +28,34 @@ class _MiniatureBoxElementState extends State<MiniatureBoxElement> {
 
   _MiniatureBoxElementState(this.captured);
 
+  void changeCaptureState() {
+    captured ?
+    Repository.get().releasePokemon(widget.livingdexId, widget.pokemon.id):
+    Repository.get().capturePokemon(widget.livingdexId, widget.pokemon.id);
+
+    setState(() {
+      captured = !captured;
+    });
+
+    widget.onCaptureStatusChange(captured);
+  }
 
   Widget build(BuildContext context) {
     final pokemon = widget.pokemon;
-    return Container(
-        //width: 20,
-        //height: 20,
-        //child: Card(
-          color: this.captured ? Colors.amber : Colors.white,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Image.asset(widget.isShiny ? pokemon.shinySpriteUrl : pokemon.spriteUrl),
-              ),
-            ],
-          ),
-        //),
+    return GestureDetector(
+      onTap: changeCaptureState,
+      child: Container(
+            color: this.captured ? Colors.amber : Colors.white,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Image.asset(widget.isShiny ? pokemon.shinySpriteUrl : pokemon.spriteUrl),
+                ),
+              ],
+            ),
+      ),
     );
   }
 }
